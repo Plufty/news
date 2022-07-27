d0a36b4567a803d328a63b3d96dd3824f4d5cc4a
Plufty <gleidson_dm@hotmail.com>
2022-07-27 18:33:33 -0300

    Atualização Banco

--- SQL Alteração no Banco.sql
+++ SQL Alteração no Banco.sql
@@ -1,8 +1,9 @@
-ALTER TABLE usuarios
-ADD COLUMN Senha varchar(30) AFTER ID;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-ALTER TABLE usuarios
-ADD COLUMN Countpts bool;
+
+
 
 DROP TABLE IF EXISTS `avaliacao`;
 CREATE TABLE IF NOT EXISTS `avaliacao` (
@@ -10,5 +11,31 @@ CREATE TABLE IF NOT EXISTS `avaliacao` (
   `ID_Usuario` int(11) NOT NULL,
   `ID_Noticia` int(11) NOT NULL,
   `Avalia` tinyint(1) DEFAULT NULL,
+  `Fake` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `noticias`;
+CREATE TABLE IF NOT EXISTS `noticias` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Titulo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Texto` varchar(3000) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Imagem` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ID_Usuario` int(11) NOT NULL,
+  `Fake` tinyint(1) NOT NULL,
+  `Pontos` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Senha` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `Nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Pontos` int(11) NOT NULL,
+  `Countpts` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
